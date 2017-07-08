5b18d6ff7a9518d06935c40295117828488f1113
justgo129 <justgo129@users.noreply.github.com>
2017-07-08 19:52:41 -0400

    Used subsetted version of data
    (Guarantees more accurate results in order to avoid escaping slashes in dates)

--- plot1.R
+++ plot1.R
@@ -3,21 +3,15 @@
 
 #Preprocessing of data
 
-powerc<-read.csv ("household_power_consumption.txt", header=TRUE, sep=";")
+#Read in Data to R
+powerc<-read.csv ("household_power_consumption_subset.txt", header=TRUE, sep="")
 powerc[powerc == "?"]<-NA   # replace all "?" fields with "NA"
-library(lubridate); library(dplyr);library(tidyr); library(graphics)
-powerc[,1]<-as.Date(powerc[,1])  # Convert dates so recognized as numeric
-date1<-filter(powerc, Date == "0001-02-07", label=TRUE)
-date2<-filter(powerc, Date == "0002-02-07", label=TRUE)
-dates<-rbind(date1,date2)
-rm(powerc)  # just to save space
+library(lubridate); library(dplyr); library(tidyr); library(graphics)
 
 
 # Create plot1.png
-dates$Global_active_power<-as.numeric(dates[,3]) # In order to avoid "x must 
-                                                # be numeric" errors 
-                                                 # when drawing the histogram
-hist(dates$Global_active_power, breaks=10,  col = "red", 
+
+hist(powerc$Global_active_power, breaks=10,  col = "red", 
      main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 
 # Save plot
