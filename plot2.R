3993c11742769e9fd2c53ca8dbfddb457847129d
justgo129 <justgo129@users.noreply.github.com>
2017-07-08 20:01:15 -0400

    Used subsetted version of data

--- plot2.R
+++ plot2.R
@@ -1,26 +1,21 @@
 #Plot 2.R
 
+#Preprocessing of data
+
 #Read in Data to R
-powerc<-read.csv ("household_power_consumption.txt", header=TRUE, sep=";")
+powerc<-read.csv ("household_power_consumption_subset.txt", header=TRUE, sep="")
 powerc[powerc == "?"]<-NA   # replace all "?" fields with "NA"
 library(lubridate); library(dplyr); library(tidyr); library(graphics)
-powerc[,1]<-as.Date(powerc[,1])  # Convert dates so recognized as numeric
-date1<-filter(powerc, Date == "0001-02-07", label=TRUE)
-date2<-filter(powerc, Date == "0002-02-07", label=TRUE)
-dates<-rbind(date1,date2)
-rm(powerc)  # just to save space
 
-#Preprocess
-
-# Plot2: Get day of week to use for x-axis labels
-daysofweek<-wday(dates$Date, label=TRUE, abbr=TRUE)  # Convert dates to days of week
-daysofweek<-factor(daysofweek, ordered = FALSE)
-daysofweek<-lapply(daysofweek, as.character)
 
 # Plot 2: Generate Plot
-plot(dates$Global_active_power, type="l", col="black", 
-     axes=T, ann=T, xlab="", xaxt="n",
-     ylab="Global Active Power (kilowatts)", lwd=2)
-axis(1, at=1:11520, labels=daysofweek)
+plot(powerc[,2], powerc[,3], type="l", main="Plot 2", 
+     xlab="", xaxt="n",
+     ylab="Global Active Power (kilowatts)")
+lines(powerc[,2], powerc[,3], type="l", col = "black", lwd=2)
+k=seq(1,2160,1.5)
+axis(1, at=seq(k), labels=powerc[k,1])
+
+# Save the Plot
 dev.copy(png, "plot2.png", width=480, height=480)
 dev.off()
