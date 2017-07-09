f072a75d9199df0acfc8872692b16a90fe0a013d
justgo129 <justgo129@users.noreply.github.com>
2017-07-08 21:37:54 -0400

    new loc in file for png

--- plot1.R
+++ plot1.R
@@ -8,12 +8,11 @@ powerc<-read.csv ("household_power_consumption_subset.txt", header=TRUE, sep="")
 powerc[powerc == "?"]<-NA   # replace all "?" fields with "NA"
 library(lubridate); library(dplyr); library(tidyr); library(graphics)
 
+png(file="plot1.png",width=480,height=480)
 
 # Create plot1.png
 
 hist(powerc$Global_active_power, breaks=10,  col = "red", 
      main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 
-# Save plot
-dev.copy(png, "plot1.png", width=480, height=480)
 dev.off()
