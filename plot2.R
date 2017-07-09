91239b9c4ae0445b8aaef6e2ce39135b5d6d596d
justgo129 <justgo129@users.noreply.github.com>
2017-07-08 21:36:31 -0400

    moved png declaration

--- plot2.R
+++ plot2.R
@@ -7,6 +7,7 @@ powerc<-read.csv ("household_power_consumption_subset.txt", header=TRUE, sep="")
 powerc[powerc == "?"]<-NA   # replace all "?" fields with "NA"
 library(lubridate); library(dplyr); library(tidyr); library(graphics)
 
+png(file="plot2.png",width=480,height=480)
 
 # Plot 2: Generate Plot
 plot(powerc[,2], powerc[,3], type="l", main="Plot 2", 
@@ -16,6 +17,5 @@ lines(powerc[,2], powerc[,3], type="l", col = "black", lwd=2)
 k=seq(1,2160,1.5)
 axis(1, at=seq(k), labels=wday(powerc[k,1], label=TRUE))
 
-# Save the Plot
-dev.copy(png, "plot2.png", width=480, height=480)
 dev.off()
+
