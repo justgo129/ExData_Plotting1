cf98a13b69c9ad1c54f5cd3c9b772a4c3b6043af
justgo129 <justgo129@users.noreply.github.com>
2017-07-11 20:37:01 -0400

    Ensured proper data

--- plot1.R
+++ plot1.R
@@ -1,18 +1,23 @@
 #Plot1.R
 
 
-#Preprocessing of data
+# Read in libraries and data frame:
+library(lubridate); library(dplyr); library(graphics) 
+powerc<-read.table("household_power_consumption.txt", 
+   header=TRUE, sep=" ")
 
-#Read in Data to R
-powerc<-read.csv ("household_power_consumption_subset.txt", header=TRUE, sep="")
-powerc[powerc == "?"]<-NA   # replace all "?" fields with "NA"
-library(lubridate); library(dplyr); library(tidyr); library(graphics)
+#Begin processing data: (1) dates, and (2) item to be tracked:
+powerc<-filter(powerc, Date == "1/2/2007" | Date == "2/2/2007")
+powerc[,1]<-as.Date(powerc[,1], format = "%d/%m/%Y")
 
-png(file="plot1.png",width=480,height=480)
+powerc[,3]<-as.numeric(as.character(powerc[,3]), na.rm=TRUE)
+powerc[,3]<-as.numeric(powerc[,3],exclude = NULL) # So no 
+                                                  # return error with "hist"
 
 # Create plot1.png
+ png(file="plot1.png",width=480,height=480)
 
-hist(powerc$Global_active_power, breaks=10,  col = "red", 
-     main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
-
+hist(powerc[,3], breaks=10, col = "red", main = "Global Active Power", 
+    xlab = "Global Active Power (kilowatts)", ylim=c(0, 1200), xaxt="n")
+axis(side=1, at=c(0, 2, 4, 6))  # sets non-default x-axis values
 dev.off()
